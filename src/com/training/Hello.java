20c17f168abde9b6b417a09b6afc3af30b8c260c
jeygit123 <erjey.se@gmail.com>
2022-04-11 16:38:00 -0400

    commit

---
+++ src/com/training/Hello.java
@@ -0,0 +1,13 @@
+package com.training;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+		String nam = "Jey";
+		System.out.println(nam +" says " +" Hello Java");
+
+	}
+
+}
